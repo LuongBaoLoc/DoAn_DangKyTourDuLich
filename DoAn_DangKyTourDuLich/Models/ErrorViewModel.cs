0e24e66da2ca119f16279fd4c1711d336540963d
LuongBaoLoc <luongbaoloc2014@gmail.com>
2026-03-13 14:42:58 +0700

    modau

---
+++ DoAn_DangKyTourDuLich/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DoAn_DangKyTourDuLich.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
